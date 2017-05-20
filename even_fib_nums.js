77d2031a95be71f44de857d9d38a33301097e350
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-20 10:01:06 -1000

    I goofed... only had one commit :(

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -5,31 +5,54 @@
  * @param  {Number} maxFibValue
  * @return {Number} sum
  */
-function _sumFibs( maxFibValue ) {
-  var sum = 0;
+function _sumFibs(maxFibValue) {
 
-  // do your work here
+
+
+  var previous = 0;
+  var current = 1;
+  var sum = 0;
+  var next;
+
+  for (i = 1; i < 4000000; i++) {
+    next = current + previous;
+    previous = current;
+    current = next;
+    if (current % 2 === 0 && current <= maxFibValue) {
+      sum += current;
+    }
+  }
+
 
   return sum;
 }
 
 // bonus round
-function _highestFibonacciNumber (maxFibValue){
+function _highestFibonacciNumber(maxFibValue) {
   var highest = 0;
-
-  //define your base case, validate your input
-
-
-  //do your work here
+  var previous = 0;
+  var current = 1;
+  var next;
+  for (var i = 0; i < 100; i++) {
+    next = current + previous;
+    previous = current;
+    current = next;
+    if (current <= maxFibValue) {
+      highest=current
+    }
+  }
 
   return highest;
+
 };
+_highestFibonacciNumber(13);
+  console.log(_highestFibonacciNumber(13));
 
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
  */
 module.exports = {
-  sumFibs : _sumFibs,
-  highestFibonacciNumber : _highestFibonacciNumber
+  sumFibs: _sumFibs,
+  highestFibonacciNumber: _highestFibonacciNumber
 };
--- test/even_fib_nums_spec.js
+++ test/even_fib_nums_spec.js
@@ -16,11 +16,11 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
 
   // when the 2 tests pass, unskip the next test and
   // change the expected value to the correct result
-  it.skip( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
+  it( 'should return the sum of all even Fibonacci numbers when given 4,000,000', function() {
     var sum = fibonnaciNums.sumFibs(4000000);
 
     // CHANGE THE VALUE OF 'X' to the correct number
-    sum.should.equal( 'X' );
+    sum.should.equal( 4613732 );
   });
 });
 
@@ -33,7 +33,7 @@ describe( 'Sum of even numbers in the Fibonnacci sequence', function() {
  */
 
 // unskip the following test to test your highestFibonacciNumber() function
-describe.skip( 'Highest number in Fibonacci sequence', function() {
+describe( 'Highest number in Fibonacci sequence', function() {
   it( 'should return the highest Fibonacci number less than the given input', function() {
     var highest = fibonnaciNums.highestFibonacciNumber( 100 );
     highest.should.equal( 89 );
